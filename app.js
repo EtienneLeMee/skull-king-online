50215e37c5f42c9e65ccf9eb9e2271dd1f8c88f9
Etienne Le Mée <et.lemee@gmail.com>
2025-02-21 13:09:11 +0100

    feature : distribuer les cartes et les afficher par joueur (#13)

--- app.js
+++ app.js
@@ -93,8 +93,15 @@ wss.on("connection", function connection(ws) {
             let player = data.player;
             let room = rooms.find(r => r.id === player.roomId);
             room = startGame(room);
+            let roomCopy = JSON.parse(JSON.stringify(room));
+            // Supprimer les decks des autres joueurs
+            roomCopy.players.forEach(p => {
+                delete p.deck;
+            });
             room.players.forEach(p => {
-                sendData = JSON.stringify({event: event, data: {player: p, room: room}});
+                let playerCards = p.cards;
+                console.log(playerCards)
+                sendData = JSON.stringify({event: event, data: {player: p, room: roomCopy, cards: playerCards}});
                 p.ws.send(sendData);
             });
         }
@@ -162,32 +169,76 @@ function roomId() {
 function startGame(room) {
     console.log(`[SERVER] Event - startGame - ${room.id}`);
     room = initScore(room)
-    startRound(room);
+    room = startRound(room);
     return room;
 }
 
 function initScore(room){
     room.players.forEach(p => p.score = 0);
-    room.round = 1;
+    room.round = 2;
     return room
 }
 
 function startRound(room){
-    if(room.round=10){
+    room.round += 1;
+    console.log(`[SERVER] Game - Start Round - Room : ${room.round}`)
+    if(room.round==10){
         //finishGame here
     } else {
-        //Shuffle here
-    
+        console.log(`[SERVER] Game - initAndShuffleDeck - Room : ${room.id}`)
+        room.deck = initAndShuffleDeck()
+        
+        room = dealCards(room);
         //Distrib here
 
         //MakePrediction here with user action
 
         //
 
-        room.round += 1;
     }
     return room;
 } 
+
+function initAndShuffleDeck(){
+    let deck = [];
+
+    const suits = ["yellow", "red", "green", "black"];
+    const heads = [{suit: "Skull King", value:100},{suit: "Pirate", value:100},{suit: "Pirate", value:100},{suit: "Pirate", value:100},{suit: "Pirate", value:100},{suit: "Pirate", value:100},{suit: "Sirène", value:100},{suit: "Sirène", value:100},{suit: "Drapeau blanc", value:0}];
+
+    // Remplir le deck avec les cartes
+    suits.forEach((suit) => {
+        for(let i=1; i<=14;i++){
+            let value = i
+            if(suit==="black"){
+                value += 20           
+            }
+            deck.push({suit: suit, value: i})
+        }
+    });
+
+    heads.forEach(h => deck.push(h))
+
+    for (let i = deck.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        let change = deck[i]
+        deck[i] = deck[j]
+        deck[j] = change
+    }
+
+    return deck
+}
+
+function dealCards(room){
+    for(let i=0; i<room.round;i++){
+        players.forEach(p=> {
+            p.cards = []
+            p.cards.push(room.deck[0]);
+            room.deck.splice(0,1);
+        });
+    }
+    room.players.forEach(p => p.cards.forEach(card => console.log(`[SERVER] - Deal Cards - ${p.username} - ${card.suit} : ${card.value}`)))
+    return room;
+}
 
 function outputRooms() {
     console.log("Rooms");
